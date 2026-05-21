e1fd6ef49449fa15924f5486bd632fe0f7b565dc
David Millar-Durrant <dmillardurrant@gmail.com>
2026-05-21 23:27:34 +0200

    Added a nix build and .envrc

--- flake.nix
+++ flake.nix
@@ -4,11 +4,14 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay.url = "github:oxalica/rust-overlay";
+    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
   };
   outputs =
     {
       self,
       nixpkgs,
+      rust-overlay,
       flake-utils,
       ...
     }:
@@ -16,19 +19,24 @@
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ rust-overlay.overlays.default ];
+        };
 
-        # Pick what rust compiler to use
-        rustVersion = pkgs.rust-bin.stable.latest.default;
+        rustToolchain = pkgs.rust-bin.stable."1.86.0".default.override {
+          targets = [ "wasm32-unknown-unknown" ];
+        };
       in
       {
         devShell = pkgs.mkShell {
 
           buildInputs = with pkgs; [
-            cargo
+            rustToolchain
             cargo-watch
             cargo-audit
             rust-analyzer
+            binaryen
             llvmPackages.clang
           ];
         };
